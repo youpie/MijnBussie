ed6d6f7d3810ef0c7f95e6acfe289a543d5f1bf4
youpie <youplamb@hotmail.nl>
2025-11-21 21:58:23 +0100

    fixed timer

--- src/execution/timer.rs
+++ src/execution/timer.rs
@@ -37,28 +37,44 @@ pub fn get_system_time() -> Time {
     time
 }
 
-#[allow(dead_code, unused_variables)]
 pub async fn calculate_next_execution_time(data: Arc<RwLock<UserData>>, first_time: bool) -> Time {
     let mut current_system_time = get_system_time();
     if let Ok(zerod_system_time) = current_system_time.replace_second(0) {
         current_system_time = zerod_system_time;
     }
     let user_properties = &data.read().await.user_properties;
-    let interval = user_properties.execution_interval_minutes;
+    let mut interval_hours = user_properties.execution_interval_minutes / 60;
+    if interval_hours == 0 {
+        interval_hours += 1
+    }
+    let execution_minute = user_properties.execution_minute;
+    _ = user_properties;
 
     let execution_time = if first_time {
-        let execution_minute = user_properties.execution_minute;
-        let random_execution_hour = rand::random_range(0..=interval / 3600);
+        let random_execution_hour = rand::random_range(0..=interval_hours);
 
         let mut execution_time =
             current_system_time + Duration::hours(random_execution_hour.into());
 
-        if let Ok(adjusted_start) = execution_time.replace_minute(execution_minute as u8) {
+        if let Ok(adjusted_start) = execution_time.replace_minute(execution_minute as u8)
+            && (current_system_time.minute() < execution_minute as u8 || random_execution_hour != 0)
+        {
+            execution_time = adjusted_start
+        } else if let Ok(adjusted_start) =
+            execution_time.replace_minute(current_system_time.minute() + 1)
+        {
             execution_time = adjusted_start
         }
         execution_time
     } else {
-        current_system_time + Duration::minutes(interval.into())
+        let next_execution_time = current_system_time + Duration::hours(interval_hours.into());
+        if let Ok(execution_time_min_adj) =
+            next_execution_time.replace_minute(execution_minute as u8)
+        {
+            execution_time_min_adj
+        } else {
+            next_execution_time
+        }
     };
 
     execution_time
@@ -77,8 +93,11 @@ pub async fn execution_timer(instances: Arc<RwLock<InstanceMap>>) -> GenResult<(
 
         let instances = &mut *instances.write().await;
         let current_system_time = get_system_time();
+        let system_time_hm = (current_system_time.hour(), current_system_time.minute());
         for instance in instances.iter_mut() {
-            if instance.1.execution_time <= current_system_time {
+            let instance_execution = instance.1.execution_time;
+            let instance_time_hm = (instance_execution.hour(), instance_execution.minute());
+            if instance_time_hm == system_time_hm {
                 let user_name = instance.0;
                 debug!("Starting instance {user_name}");
                 _ = instance.1.request_sender.try_send(StartRequest::Timer);
